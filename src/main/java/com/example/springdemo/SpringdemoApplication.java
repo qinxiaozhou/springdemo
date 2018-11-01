85b4b7d4447dd1ddd3d9763bc4bc7140db67c066
qinxiaohou <week.qin@163.com>
2018-11-01 11:34:22 +0800

    file modify

--- src/main/java/com/example/springdemo/SpringdemoApplication.java
+++ src/main/java/com/example/springdemo/SpringdemoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringdemoApplication {
 
 	public static void main(String[] args) {
-		System.out.println(123213);
+		System.out.println(5646);
 		SpringApplication.run(SpringdemoApplication.class, args);
 	}
 }
